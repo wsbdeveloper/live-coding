310c5a46d53e1318404b5d66013b8eb70fd9d11b
wsbengineer <wellington@up4tech.com.br>
2023-07-21 18:24:23 -0300

    finish the api data / initiate infra database

---
+++ live_coding/src/infra/databases/alembic/versions/a001cd0020af_model_example.py
@@ -0,0 +1,24 @@
+"""model example
+
+Revision ID: a001cd0020af
+Revises: 
+Create Date: 2023-07-21 08:58:56.646151
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a001cd0020af'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
